a8a83ac1d85fed57dcf4a370c43a59de58dbae7e
iceStone <ice@micua.com>
2017-05-30 17:20:50 +0800

    backup

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+(window => {
+  // const $ = window.jQuery
+  // const $window = $(window)
+  // const $navbar = $('.reborn-navbar')
+
+  // $window.on('scroll', () => {
+  //   console.log($window.scrollTop())
+  //   if ($window.scrollTop() > 1) {
+  //     $navbar.removeClass('navbar-inverse').addClass('navbar-light')
+  //   } else {
+  //     $navbar.removeClass('navbar-light').addClass('navbar-inverse')
+  //   }
+  // })
+})(window)
